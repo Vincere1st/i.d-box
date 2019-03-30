f26eb4cdc610644903553f7a2f7db8c806e77d97
jiceR <jc-roger45000@protonmail.com>
2019-03-30 04:02:17 +0100

    update form modal

--- public/modal.php
+++ public/modal.php
@@ -7,34 +7,107 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-
+    <link href="https://fonts.googleapis.com/css?family=Gloria+Hallelujah" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
     <title>Hello, world!</title>
 </head>
-<body>
-<h1>Hello, world!</h1>
 
+<h1>I.d Box</h1>
+<?php
+require '../connec.php';
+include '../src/functions.php';
+$errors = [];
+$pdo = new PDO(DSN, USER, PASS);
+$pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $data = cleanInput($_POST);
+
+    if (empty($data["firstname"])) {
+        $errors["firstname"] = "Entrez votre nom";
+    }
+    if (empty($data["title"])) {
+        $errors["title"] = "Entrez un titre";
+    }
+    if (empty($data["lastname"])) {
+        $errors["lastname"] = "Entrez votre nom";
+    }
+    if (empty($data["content"])) {
+        $errors["content"] = "Veuillez entrer votre idée";
+    }
+echo $data["content"];
+    if (empty($errors)) {
+
+        $query = "INSERT INTO idea (title, lastname, firstname, content, category)
+      VALUES (:title, :lastname, :firstname, :content, :category)";
+        $statement = $pdo->prepare($query);
+        $statement->bindValue(':title', $data['title'], PDO::PARAM_STR);
+        $statement->bindValue(':lastname', $data['lastname'], PDO::PARAM_STR);
+        $statement->bindValue(':firstname', $data['firstname'], PDO::PARAM_STR);
+        $statement->bindValue(':content', $data['content'], PDO::PARAM_STR);
+        $statement->bindValue(':category', $data['category'], PDO::PARAM_STR);
+        $statement->execute();
+    }
+
+}
+?>
 <!-- Button trigger modal -->
-<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+<!--<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
     Launch demo modal
-</button>
+</button>-->
+<!--<i class="fas fa-lightbulb"></i>-->
+<a href="#exampleModal" data-toggle="modal" data-target="#exampleModal"><i class="fas fa-lightbulb"></i></a>
 
 <!-- Modal -->
 <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                <h5 class="modal-title" id="exampleModalLabel">La boite à idée</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
             </div>
             <div class="modal-body">
-                ...
+                <form method="post">
+                    <div class="form-group">
+                        <label for="firstname">Prénom</label>
+                        <span class="error">* <?php if (isset($errors["firstname"])){echo $errors["firstname"];}?></span>
+                        <input type="text" class="form-control" id="firstname" placeholder="John" name="firstname">
+                    </div>
+                    <div class="form-group">
+                        <label for="lastname">Nom</label>
+                        <span class="error">* <?php if (isset($errors["lastname"])){echo $errors["lastname"];}?></span>
+                        <input type="text" class="form-control" id="lastname" placeholder="Dupont" name="lastname">
+                    </div>
+                    <div class="form-group">
+                        <label for="categorie">Choisi ta catégorie</label>
+                        <select class="form-control" id="categorie" name="category">
+                            <option>La vie a la Wild</option>
+                            <option>Evènements entre Wilders</option>
+                            <option>Sujet de veille</option>
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <label for="title">Titre</label>
+                        <span class="error">* <?php if (isset($errors["title"])){echo $errors["title"];}?></span>
+                        <input type="text" class="form-control" id="title" placeholder="Trouvez une idée de titre ;)" name="title">
+                    </div>
+                    <div class="form-group">
+                        <label for="content">Example textarea</label>
+                        <span class="error">* <?php if (isset($errors["content"])){echo $errors["content"];}?></span>
+                        <textarea class="form-control" id="content" rows="3" name="content" placeholder="Propose ton idéé"></textarea>
+                    </div>
+                    <div class="form-group">
+                        <label for="content">Example textarea</label>
+                        <textarea class="form-control" id="content" rows="3" name="" ></textarea>
+                    </div>
+                    <button type="submit" class="btn btn-idea">Soumettre l'idée</button>
+                </form>
             </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-primary">Save changes</button>
-            </div>
+            <!--<div class="modal-footer">
+                <button type="button" class="btn btn-idea">Soumettre l'idée</button>
+            </div>-->
         </div>
     </div>
 </div>
